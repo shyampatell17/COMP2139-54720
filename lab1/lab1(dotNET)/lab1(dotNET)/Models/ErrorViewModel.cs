b8bcdb7007f4d1bc719e9291851532ec49d0f3c6
Shyam Patel <134675414+shyampatell17@users.noreply.github.com>
2024-01-27 02:56:58 -0500

    Add files via upload

---
+++ lab1/lab1(dotNET)/lab1(dotNET)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace lab1_dotNET_.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
